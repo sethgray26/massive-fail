c6276c51c1c9fedcff6fb37c2b2837b128cd7c89
Jason Staten <jasonstaten@jstaten-ml-6.local>
2018-12-19 11:05:43 -0700

    typos

--- schema.sql
+++ schema.sql
@@ -4,26 +4,26 @@ CREATE TABLE movies
   id SERIAL PRIMARY KEY,
   title TEXT,
   director TEXT,
-  year INTEGER,
-  minutes INTEGER
+  year integer,
+  minutes integer
 );
 
-INSERT INTO movies(name)
+INSERT INTO movies(title, director, year, minutes)
 VALUES
-("Toy Story","John Lasseter","1995","81"),
-("A Bug's Life","John Lasseter","1998","95"),
-("Toy Story 2","John Lasseter","1999","93"),
-("Monsters, Inc.","Pete Docter","2001","92"),
-("Finding Nemo","Andrew Stanton","2003","107"),
-("The Incredibles","Brad Bird","2004","116"),
-("Cars","John Lasseter","2006","117"),
-("Ratatouille","Brad Bird","2007","115"),
-("WALL-E","Andrew Stanton","2008","104"),
-("Up","Pete Docter","2009","101"),
-("Toy Story 3","Lee Unkrich","2010","103"),
-("Cars 2","John Lasseter","2011","120"),
-("Brave","Brenda Chapman","2012","102"),
-("Monsters University","Dan Scanlon","2013","110");
+('Toy Story','John Lasseter','1995','81'),
+('A Bugs Life','John Lasseter','1998','95'),
+('Toy Story 2','John Lasseter','1999','93'),
+('Monsters, Inc.','Pete Docter','2001','92'),
+('Finding Nemo','Andrew Stanton','2003','107'),
+('The Incredibles','Brad Bird','2004','116'),
+('Cars','John Lasseter','2006','117'),
+('Ratatouille','Brad Bird','2007','115'),
+('WALL-E','Andrew Stanton','2008','104'),
+('Up','Pete Docter','2009','101'),
+('Toy Story 3','Lee Unkrich','2010','103'),
+('Cars 2','John Lasseter','2011','120'),
+('Brave','Brenda Chapman','2012','102'),
+('Monsters University','Dan Scanlon','2013','110');
 
 
 DROP TABLE IF EXISTS creditcards;
@@ -31,7 +31,7 @@ CREATE TABLE creditcards
 (
   id SERIAL PRIMARY KEY,
   number TEXT,
-  expiration TEXT,
+  expiration TEXT
 );
 
 INSERT INTO creditcards (number, expiration)
@@ -46,7 +46,7 @@ CREATE TABLE users
   id SERIAL PRIMARY KEY,
   email TEXT,
   password TEXT,
-  passwordHash TEXT,
+  passwordHash TEXT
 );
 
 INSERT INTO users (email, password, passwordHash)
